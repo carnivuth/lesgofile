cbc9f52f8418534b99757f2e298de582154bf362
carnivuth <matti200042@gmail.com>
2023-02-05 17:48:38 +0100

    added settings loader

---
+++ settingsLoader.go
@@ -0,0 +1,32 @@
+package lesGoFile
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const SETTINGS_FILE = "settings.conf"
+
+var SETTINGS map[string]string = make(map[string]string)
+
+func getSettings() {
+
+	readFile, err := os.Open(SETTINGS_FILE)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+	//load settings
+	for fileScanner.Scan() {
+		setting := strings.Split(fileScanner.Text(), ":")
+		SETTINGS[setting[0]] = setting[1]
+	}
+	//close file
+	readFile.Close()
+}
