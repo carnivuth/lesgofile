8946091c292eddac52a85231e92a306aedcae110
carnivuth <matti200042@gmail.com>
2023-02-05 23:46:44 +0100

    fixed settings copy

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir build >/dev/null 2>/dev/null
-cp settings.conf build/setting.conf
-go build -o build/lesGoFile
+cp settings.conf build/settings.conf
+go build -o build/lesgofile.o
