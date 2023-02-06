0c262f08eb28ae5f475b859cd27160544895fcec
carnivuth <matti200042@gmail.com>
2023-02-06 18:26:41 +0100

    added network protocol

--- network/client.go
+++ network/client.go
@@ -1,17 +1,45 @@
 package network
 
 import (
+	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
+	"os"
+	"strconv"
+
+	"github.com/carnivuth/lesgofile/settings"
 )
 
 // connect to server and upload file
-func Sender(address string, port string) {
+func Sender(address string, port string, filename string) {
+	dim, err := strconv.Atoi(settings.SETTINGS["DIM_BUFFER"])
+	buffer := make([]byte, dim)
+	//connect to server
 	conn, err := net.Dial("tcp", address+":"+port)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Println("connection succesful to host: " + address)
+	//open file
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	binary.Write(conn, binary.BigEndian, int64(len(filename)))
+
+	//send filename
+	conn.Write([]byte(filename))
+
+	//send file
+	for err != io.EOF {
+		var n_read int
+		n_read, err = file.Read(buffer)
+		conn.Write(buffer[:n_read])
+
+	}
+	file.Close()
 	conn.Close()
 }
--- network/server.go
+++ network/server.go
@@ -1,8 +1,14 @@
 package network
 
 import (
+	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
+	"os"
+	"strconv"
+
+	"github.com/carnivuth/lesgofile/settings"
 )
 
 //wait for client and download file
@@ -21,16 +27,45 @@ func Reciver(port string, terminate chan int) {
 			fmt.Println("unable to accept request from client")
 		} else {
 			//handle connection with go routine
-			go saveFile(&conn, terminate)
+			go saveFile(conn, terminate)
 			<-terminate
 			fmt.Println("connection terminated ")
 		}
 	}
 
 }
-func saveFile(conn *net.Conn, terminate chan int) {
+func saveFile(conn net.Conn, terminate chan int) {
+	var size int64
+	var n_read int
+	dim, err := strconv.Atoi(settings.SETTINGS["DIM_BUFFER"])
+	buffer := make([]byte, dim)
+	filename := make([]byte, dim)
 	fmt.Println("connected to client")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	binary.Read(conn, binary.BigEndian, &size)
+
+	//read filename
+	n_read, err = conn.Read(filename[:size])
+	fmt.Printf("recived %s filename\n", string(filename[:n_read]))
+
+	//create file
+	file, err := os.Create(string(filename[:n_read]))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//write file
+	for err != io.EOF {
+		n_read, err = conn.Read(buffer)
+		//fmt.Printf("recived %d byte\n", n_read)
+		file.Write(buffer[:n_read])
+	}
+	file.Close()
+
 	terminate <- 1
-	///create protocol for transfer file over server
 
 }
