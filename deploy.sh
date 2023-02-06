2cbd0cc4a0c5132eb79084dcb80cb162ed829fcb
carnivuth <matti200042@gmail.com>
2023-02-06 20:55:20 +0100

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+[ "$#" -ne 3 ] && echo "parameters required GOOS GOARCH FILENAME" && exit
+
+./build.sh "$@"
+mkdir deploy >/dev/null 2>/dev/null
+tar -cf deploy/deploy-lesgofile-"$(date -I)"-"$1"-"$2".zip build/"$3" build/settings.conf 
