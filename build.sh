4a734ef30c4b23c891f0aee82d9416550bbb0c76
carnivuth <matti200042@gmail.com>
2023-02-08 18:48:02 +0100

    updated script to account settings example

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 [ "$#" -ne 3 ] && echo "parameters required GOOS GOARCH FILENAME" && exit
 
 mkdir build >/dev/null 2>/dev/null
-cp settings.conf build/settings.conf
+cp settings.conf.example build/settings.conf
 env GOOS="$1" GOARCH="$2" go build -o build/"$3"
