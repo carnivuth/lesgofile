41c88c1da896e9f0e8433b81b02d5033711a4884
carnivuth <matti200042@gmail.com>
2023-04-01 14:47:34 +0200

    added json support

--- settings/settingsLoader.go
+++ settings/settingsLoader.go
@@ -1,13 +1,13 @@
 package settings
 
 import (
-	"bufio"
+	"encoding/json"
+	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
-const SETTINGS_FILE = "settings.conf"
+const SETTINGS_FILE = "settings.json"
 
 var SETTINGS map[string]string = make(map[string]string)
 
@@ -23,15 +23,17 @@ func LoadSettings() {
 	if err != nil {
 		panic(err)
 	}
+	byteResult, _ := ioutil.ReadAll(readFile)
+	json.Unmarshal([]byte(byteResult), &SETTINGS)
 
-	fileScanner := bufio.NewScanner(readFile)
+	/*fileScanner := bufio.NewScanner(readFile)
 
 	fileScanner.Split(bufio.ScanLines)
 	//load settings
 	for fileScanner.Scan() {
 		setting := strings.Split(fileScanner.Text(), "=")
 		SETTINGS[setting[0]] = setting[1]
-	}
+	}*/
 	//close file
 	readFile.Close()
 }
