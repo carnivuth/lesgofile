827a4842d842327936e8836a51f9b2a9a897e37f
carnivuth <matti200042@gmail.com>
2023-04-01 15:19:58 +0200

    added json support

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 [ "$#" -ne 3 ] && echo "parameters required GOOS GOARCH FILENAME" && exit
 
 mkdir build >/dev/null 2>/dev/null
-cp settings.conf.example build/settings.conf
+cp settings.example.json build/settings.json
 env GOOS="$1" GOARCH="$2" go build -o build/"$3"
