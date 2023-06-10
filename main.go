849468efd8efa9e4ae6748f25012dd6510d29148
carnivuth <matti200042@gmail.com>
2023-06-10 23:12:50 +0200

    remove address check

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"os"
 
 	"github.com/carnivuth/lesgofile/network"
@@ -20,29 +19,12 @@ func main() {
 	args := os.Args[1:]
 	if args[0] == "send" {
 
-		//check parameters and launch client
-		if checkParameters(args[1]) {
+		network.Sender(args[1], settings.SETTINGS["PORT"], args[2])
 
-			network.Sender(args[1], settings.SETTINGS["PORT"], args[2])
-
-		} else {
-
-			fmt.Println("wrong ip address: " + args[1])
-			return
-
-		}
 	} else if args[0] == "recive" {
 
 		//launch server
 		network.Reciver(settings.SETTINGS["PORT"], terminate)
 
 	}
-}
-
-func checkParameters(address string) bool {
-	if net.ParseIP(address) == nil {
-		return false
-	}
-	return true
-
 }
