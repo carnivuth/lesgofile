447e6f391471223deafdd6b5bd2abfbdccae0974
carnivuth <matti200042@gmail.com>
2023-06-10 20:58:43 +0200

    updated deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 
 ./build.sh "$@"
 mkdir deploy >/dev/null 2>/dev/null
-tar -czf deploy/deploy-lesgofile-"$(date -I)"-"$1"-"$2".tar.gz build/"$3" build/settings.json 
+zip deploy/deploy-lesgofile-"$(date -I)"-"$1"-"$2".zip build/"$3" build/settings.json 
