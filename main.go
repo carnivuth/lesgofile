61411e8f162dd8247af947f3bb2a145fe2047606
carnivuth <matti200042@gmail.com>
2023-07-24 12:57:09 +0200

    added logger and filter behaviour

--- main.go
+++ main.go
@@ -1,30 +1,57 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
 	"os"
 
+	"github.com/carnivuth/lesgofile/logger"
 	"github.com/carnivuth/lesgofile/network"
 	"github.com/carnivuth/lesgofile/settings"
 )
 
 // /check parameters and launch slaves
+
+const PARAMETERS = "PARAMETERS \nfunctionmode = [send|recive] \nSEND PARAMETERS \nserveraddress filename"
+
+// PARAMETERS
+
+// functionmode = [send|recive]
+// SEND PARAMETERS
+//
+//	serveraddress filename
+func printToStdOut(message string) {
+	println(message)
+}
+
 func main() {
-	var terminate chan int = make(chan int)
+	logger.AddListener(logger.Log, printToStdOut)
 	settings.LoadSettings()
-	if len(os.Args) < 2 {
-		fmt.Println("too fiew arguments ")
+
+	logger.Emit(logger.Log, "settings loaded")
+
+	var args = os.Args[1:]
+	if len(args) < 1 {
+		logger.Emit(logger.Log, "too fiew arguments ")
+		logger.Emit(logger.Log, PARAMETERS)
 		return
 	}
-	args := os.Args[1:]
 	if args[0] == "send" {
+		if len(args) == 3 {
+			network.Sender(args[1], settings.SETTINGS["PORT"], args[2])
+		} else if len(args) < 3 {
+			//filter behavior
+			scanner := bufio.NewScanner(os.Stdin)
+			for scanner.Scan() {
+				network.Sender(args[1], settings.SETTINGS["PORT"], scanner.Text())
 
-		network.Sender(args[1], settings.SETTINGS["PORT"], args[2])
+			}
+
+		}
 
 	} else if args[0] == "recive" {
 
 		//launch server
-		network.Reciver(settings.SETTINGS["PORT"], terminate)
+		network.Reciver(settings.SETTINGS["PORT"])
 
 	}
 }
